ad1e1117d4378d3f6133cb7e831c563fcaaa1b07
kicks <kicks@KICKSCAR-LAPTOP>
2017-06-01 07:06:29 +0900

    init commit

---
+++ src/com/bit2015/fileupload/controller/FileUploadController.java
@@ -0,0 +1,14 @@
+package com.bit2015.fileupload.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.bit2015.fileupload.service.FileUploadService;
+
+@Controller
+public class FileUploadController {
+}
